58baf2a1699b0b7c210b6a8100345edefdb6a882
Md-suhail07-code <suhailahmedmd40@gmail.com>
2026-04-30 13:16:09 +0530

    Updated addressSlice to match database states

--- Frontend/src/redux/addressSlice.js
+++ Frontend/src/redux/addressSlice.js
@@ -8,18 +8,30 @@ const addressSlice = createSlice({
         status: 'idle'
     },
     reducers: {
-        setAddresses: (state, action) => { state.addresses = action.payload },
-        setSelectedId: (state, action) => { state.selectedId = action.payload },
+        setAddresses: (state, action) => { 
+            state.addresses = action.payload; 
+        },
+        setSelectedId: (state, action) => { 
+            state.selectedId = action.payload; 
+        },
         addAddress: (state, action) => {
             state.addresses.push(action.payload);
-            state.selectedId = action.payload.id;
+            state.selectedId = action.payload._id;
+        },
+        updateAddressState: (state, action) => {
+            const index = state.addresses.findIndex(a => a._id === action.payload._id);
+            if (index !== -1) {
+                state.addresses[index] = action.payload;
+            }
         },
         removeAddress: (state, action) => {
-            state.addresses = state.addresses.filter(a => a.id !== action.payload)
-            if (state.selectedId === action.payload) state.selectedId = null
+            state.addresses = state.addresses.filter(a => a._id !== action.payload);
+            if (state.selectedId === action.payload) {
+                state.selectedId = null;
+            }
         }
     }
 });
 
-export const { setAddresses, setSelectedId, addAddress, removeAddress } = addressSlice.actions;
+export const { setAddresses, setSelectedId, addAddress, updateAddressState, removeAddress } = addressSlice.actions;
 export default addressSlice.reducer;
